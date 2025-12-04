cb226e75af7c6223894def155e52acadb6c9ae5b
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-12-04 10:51:33 +0100

    Update SLURM script for WiSe 25/26

--- slurm_script.sh
+++ slurm_script.sh
@@ -7,25 +7,25 @@
 ### Which kind of compute node do you want to allocate
 #SBATCH --mem-per-cpu=3000
 #SBATCH -N 1
-#SBATCH -n 104
-#SBATCH -c 1
+#SBATCH -n 1
+#SBATCH -c 96
 #SBATCH -C avx512
-#SBATCH -C i02
+#SBATCH -C i01
 ##SBATCH --exclusive
 
 ### Frequency pinning
-#SBATCH --cpu-freq=High-High
+#SBATCH --cpu-freq=HighM1
 
 ### Data for our project
-#SBATCH -p kurs00089
-#SBATCH -A kurs00089
-#SBATCH --reservation=kurs00089
+#SBATCH -p kurs00095
+#SBATCH -A kurs00095
+#SBATCH --reservation=kurs00095
 
-module purge
-ml gcc/13
+spack env activate /work/home/kurse/kurs00095/task3-spackenv
 
-srun -n 1 lscpu | grep MHz
+srun bash -c "sleep 2 && lscpu -e"
 cd build
-srun -n 1 --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
-srun -n 1 --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
-srun -n 1 --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
+
+srun --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
+srun --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
+srun --cpus-per-task=16 /usr/bin/time ./minicfd -d 100 -e 6 -s 0.4
